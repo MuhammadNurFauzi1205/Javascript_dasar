dbc0803ddd6124d59a4b38f92d99e05b68908725
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2023-09-01 13:09:51 +0700

    operator dan tipe data
    berlatih operator dan tipe data

---
+++ Operator.js
@@ -0,0 +1,9 @@
+console.log(15+1); //operator aritmatika +,-,*,/,%
+x = 5;
+console.log(x); //operator assignment =
+console.log(5 == 5); //operator perbandingan ==, !=. ===, !==, >, <, >=, <=
+console.log(5 === "5"); // membandingkan dengan tipe data
+a = 12;
+console.log(a % 2 == 0); //operator logika &&, ||, !
+console.log((a % 2 == 0) && (a<10)); // a = 12 itu bilangan genap maka true, kemudian jika a=12 < 10 maka false
+console.log(typeof(a)); //operator typeof
---
+++ Tipe_Data.js
@@ -0,0 +1,11 @@
+console.log(999999999999999);
+// jika lebih dari 15 digit angkanya akan dibulat kan dengan contoh dibawah menjadi 10000000000000000
+console.log(9999999999999999);
+//eksponen
+console.log(123e5); //12300000
+console.log(123e-5); //0.00123
+console.log(0xff); //bilangan hexa menjadi 255
+console.log(022); //bilangan oktal dan jangan depanya 0 kare konv ke okt
+console.log(0/0); //NaN (Not an Number)
+console.log(1/0); //infinity
+console.log(1/-0); //-infinity
