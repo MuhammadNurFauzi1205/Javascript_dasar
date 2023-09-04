183f173fe0cf3242b701c85a2cf228b63656e081
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2023-09-04 20:34:42 +0700

    Update Bintang.js
    membuat game bintang

--- Bintang.js
+++ Bintang.js
@@ -7,8 +7,16 @@ for( var i = 0 ; i<5; i++){
 }
 console.log(bintang);*/
 
-var bintang = '';
+/*var bintang = '';
 for( var i = 0 ; i<5; i++){
     bintang += '*'
     console.log(bintang);
+}*/
+var bintang = '';
+for( var baris = 0 ; baris<5; baris++){
+    for(var kolom = 0 ; kolom<=2; kolom++){
+        bintang += '*';
+    }
+    bintang +='\n';
 }
+console.log(bintang);
