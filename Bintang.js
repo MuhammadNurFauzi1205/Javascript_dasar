fcd9d6358ba8d4ea58270bae1633bd203150973f
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2023-09-04 20:36:29 +0700

    Update Bintang.js
    membuat game bintang, dengan berbagai macam variasi

--- Bintang.js
+++ Bintang.js
@@ -12,9 +12,19 @@ for( var i = 0 ; i<5; i++){
     bintang += '*'
     console.log(bintang);
 }*/
-var bintang = '';
+
+/*var bintang = '';
 for( var baris = 0 ; baris<5; baris++){
     for(var kolom = 0 ; kolom<=2; kolom++){
+        bintang += '*';
+    }
+    bintang +='\n';
+}
+console.log(bintang);*/
+
+var bintang = '';
+for( var baris = 0 ; baris<5; baris++){
+    for(var kolom = 0 ; kolom<=baris; kolom++){
         bintang += '*';
     }
     bintang +='\n';
