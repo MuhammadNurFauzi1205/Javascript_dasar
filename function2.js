30700d30fefe13bc85373620df5cd039216d7615
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2023-09-21 23:21:22 +0700

    function2.js dan tempCoderunnerfile
    coba loop di dalam fungsi

---
+++ function2.js
@@ -0,0 +1,10 @@
+let data;
+
+function x() {
+    for(let i= 0; i<=4; i++){
+        data=i;
+        
+    }
+    return data;
+}
+console.log(x());
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let data;
+
+function x() {
+    if(data !== null || data !== undefined){
+        return true;
+    } else{
+        return false;
+    }
+}
+console.log(x())
